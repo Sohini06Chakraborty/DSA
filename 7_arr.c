6d083cad5abd2b7d6d4b3cf84b20c1046abddfcf
Sohini06Chakraborty <sohini2608@gmail.com>
2025-09-09 00:52:39 +0530

    Correct solution

--- 7_arr.c
+++ 7_arr.c
@@ -1,15 +1,10 @@
 //You are given an integer array prices where prices[i] is the price of a given stock on the ith day. On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day. Find and return the maximum profit you can achieve.
 #include<stdio.h>
 int maxProfit(int* prices, int pricesSize) {
-    int min_price = prices[0];
     int max_profit = 0;
     for(int i = 1; i < pricesSize; i++) {
-        if(min_price > prices[i]) {
-            min_price = prices[i];
-        }
-        int current_profit = prices[i] - min_price;
-        if(current_profit > max_profit) {
-            max_profit = current_profit;
+        if(prices[i] > prices[i-1]) {
+            max_profit += prices[i] - prices[i-1];
         }
     }
     return max_profit;
